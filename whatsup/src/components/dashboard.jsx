62464b7abeb9a1127abf964b2e92943415b48d43
gouravsahuu <gsgourav.sahu@gmail.com>
2023-10-06 22:54:04 +0530

    created dashboard page

---
+++ whatsup/src/components/dashboard.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+function Dashboard() {
+
+    const url = "";
+
+    let [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            setUserData(data);
+        })
+    },[])
+
+    return (
+        <>
+            
+        </>
+    );
+}
