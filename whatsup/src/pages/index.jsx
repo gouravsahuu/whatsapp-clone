4738fb8f13830e43b5474d6bd00deaf12d0267a8
gouravsahuu <gsgourav.sahu@gmail.com>
2023-10-10 23:26:52 +0530

    created index page

---
+++ whatsup/src/pages/index.jsx
@@ -0,0 +1,20 @@
+function Index() {
+    return <>
+        <nav>
+            <img src="" alt="" />
+            <div>
+                <p>Login</p>
+                <p>Signup</p>
+            </div>
+        </nav>
+        <div>
+            <div>Message securely</div>
+            <div>High quality calls</div>
+            <div>Share any kind of documents</div>
+            <div>One time message</div>
+        </div>
+        <div>
+            Download our app
+        </div>
+    </>
+}
