918e11bc7946182c00076219754575dafbe515bc
gouravsahuu <gsgourav.sahu@gmail.com>
2023-10-11 23:35:35 +0530

    created signup page

---
+++ whatsup/src/pages/signup.jsx
@@ -0,0 +1,18 @@
+function Signup() {
+    return <>
+        <nav>
+            <img src="" alt="" />
+            <div>
+                <p>Login</p>
+            </div>
+        </nav>
+        <div>
+            <form>
+                <input type="text" placeholder="Enter Username" />
+                <input type="email" placeholder="Enter email" />
+                <input type="password" placeholder="Choose a strong password" />
+                <input type="submit" value="Signup" />
+            </form>
+        </div>
+    </>
+}
