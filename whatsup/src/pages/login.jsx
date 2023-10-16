fb9ac751dd1cd1588641d961105d0e9ba94fbf31
gouravsahuu <gsgourav.sahu@gmail.com>
2023-10-16 23:14:37 +0530

    updated

--- whatsup/src/pages/login.jsx
+++ whatsup/src/pages/login.jsx
@@ -1,8 +1,17 @@
 function LoginPage() {
     return <>
         <nav>
-            <p>WhatsUp</p>
+            <img src="" alt="" />
+            <div>
+                <p>Signup</p>
+            </div>
         </nav>
-        
+        <div>
+            <form>
+                <input type="email" placeholder="Enter email" />
+                <input type="password" placeholder="Enter password" />
+                <input type="submit" value="Login" />
+            </form>
+        </div>
     </>
 }
